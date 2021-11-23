c538b47bb2840601bab55ba0762ce2d791283fa4
Artur Ribeiro <profissional.arturrc@gmail.com>
2021-11-23 10:43:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+
+const App = () => {
+  const dados = luana;
+
+  const total = dados.compras
+    .map((item) => Number(item.preco.replace("R$ ", "")))
+    .reduce((a, b) => a + b);
+
+  //Colore se ativo ou inativo.
+  function colorText() {
+    return { color: dados.ativa ? "green" : "red" };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <p> Nome: {dados.cliente}</p>
+      <p>idade: {dados.idade}</p>
+
+      <p>
+        Status conta:{" "}
+        <span style={colorText()}>{dados.ativa ? "Ativo" : "Inativo"} </span>
+      </p>
+
+      <p>
+        Total:{" "}
+        <span style={{ color: total < 10000 ? "green" : "red" }}>
+          {" "}
+          R$ {total}
+        </span>
+      </p>
+
+      {total > 10000 ? (
+        <p style={colorText()}>Você está gastando demais, controle-se </p>
+      ) : (
+        <p style={colorText()}>
+          Você está controlando bem seus gastos, continue assim!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
-}
+};
 
 export default App;
